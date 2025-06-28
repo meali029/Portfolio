1c7d8cb2ebf1d6ea5219f6596440f5cbe54196ba
Mehboob Ali <mehboobaliali150@gmail.com>
2025-06-28 15:55:16 +0500

    connect page btn text update

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -44,7 +44,7 @@ const Contact = ({ darkMode }) => {
             target="_blank"
             rel="noopener noreferrer"
             className={`relative inline-flex items-center gap-3 px-6 py-3 rounded-full shadow-lg overflow-hidden transition duration-200 ${
-              darkMode ? "bg-blue-600 hover:bg-blue-500" : "bg-blue-600 hover:bg-blue-700"
+              darkMode ? "bg-blue-600 hover:bg-blue-500" : "bg-blue-600 text-white hover:bg-blue-700"
             }`}
           >
             <Linkedin size={24} />
